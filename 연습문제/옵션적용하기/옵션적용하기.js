f539a8521762132bfc86978e52374ac449a562e2
2Seongmin <tjdrmffktm0928@gmail.com>
2025-01-10 17:44:21 +0900

    function_study

---
+++ 연습문제/옵션적용하기/옵션적용하기.js
@@ -0,0 +1,22 @@
+// input 너비, 높이, 글자크기, 굵기
+const inputs = document.querySelectorAll(".input");
+
+const boxWidth = document.querySelector("#boxWidth");
+const boxHeight = document.querySelector("#boxHeight");
+
+const btn = document.querySelector("#applyBtn");
+
+const result = document.querySelector("#result");
+
+// 적용 버튼 클릭 시
+btn.addEventListener("click", function(){
+
+    result.style.width = boxWidth.value + "px";
+    result.style.height = boxHeight,value + "px";
+
+    // 글자 굵기
+    const fw = document.querySelector("[name=font-weight]:checked");
+    // name 속성이 fw인 요소 중 체크가 되어있는 요소 1개만 선택
+    // :checked -> 체크된 요소만 선택(상태 선택자);
+
+});
